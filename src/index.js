c2675d67353d80c1031bc92f32212147b45573a3
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-25 12:54:00 +0300

    files updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { GlobalStyle } from 'components/GlobalStyle';
 import { Example1 } from 'components/Example1';
 import './index.css';
 import videos from './videos.json';
-
 // import App from './App';
 
 const theme = {};
